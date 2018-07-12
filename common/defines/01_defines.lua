5c81cfb6fa4abaf5618b53036cb34b255678e6df
fluffy <reginaldlfluffy@gmail.com>
2018-07-13 03:31:07 +1200

    first commit

---
+++ common/defines/01_defines.lua
@@ -0,0 +1,21 @@
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0;
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0;
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0;
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 72;			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5;			-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.ARMY_LEADER_XP_GAIN_PER_UNIT_IN_COMBAT = 0.05; -- XP gain per unit in combat
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1;			   -- Base xp gain for traits per hour.
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000;			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000;			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000;				--Max air experience a country can store
+
+
+NDefines.NDiplomacy.BASE_STAGE_COUP_DAILY_PP = 2001;					-- Daily pp cost for staging a coup
+NDefines.NDiplomacy.BASE_STAGE_COUP_TOTAL_COST = 200000; 				-- Equipment consume factor for stage coup.
+NDefines.NDiplomacy.IC_TO_EQUIPMENT_COUP_RATIO = 1;			-- Ratio for calculating cost of staging coup
+
+NDefines.NPolitics.ARMY_LEADER_COST = 1;					-- cost for recruiting new leaders, 'this value' * number_of_existing_leaders_of_type
+NDefines.NPolitics.NAVY_LEADER_COST = 1;   				-- cost for recruiting new leaders, 'this value' * number_of_existing_leaders_of_type
+
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50
