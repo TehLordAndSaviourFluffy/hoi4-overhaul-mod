0715cdf0d26037f3cc9d727fc99138cf99f26a2f
Martijn ten Veldhuis <mj.ten.veldhuis@live.nl>
2018-07-13 17:30:43 +0200

    Jay's define changes from jfu

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -2,20 +2,44 @@ NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0;
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0;
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0;
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 72;			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5;			-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.ARMY_LEADER_XP_GAIN_PER_UNIT_IN_COMBAT = 0.05; -- XP gain per unit in combat
-NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1;			   -- Base xp gain for traits per hour.
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000;			--Max army experience a country can store
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000;			--Max navy experience a country can store
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000;				--Max air experience a country can store
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 72;
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5;
+NDefines.NMilitary.ARMY_LEADER_XP_GAIN_PER_UNIT_IN_COMBAT = 0.05;
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1;
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000;
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000;
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000;
 
 
-NDefines.NDiplomacy.BASE_STAGE_COUP_DAILY_PP = 2001;					-- Daily pp cost for staging a coup
-NDefines.NDiplomacy.BASE_STAGE_COUP_TOTAL_COST = 200000; 				-- Equipment consume factor for stage coup.
-NDefines.NDiplomacy.IC_TO_EQUIPMENT_COUP_RATIO = 1;			-- Ratio for calculating cost of staging coup
+NDefines.NDiplomacy.BASE_STAGE_COUP_DAILY_PP = 2001;
+NDefines.NDiplomacy.BASE_STAGE_COUP_TOTAL_COST = 200000;
+NDefines.NDiplomacy.IC_TO_EQUIPMENT_COUP_RATIO = 1;
 
-NDefines.NPolitics.ARMY_LEADER_COST = 1;					-- cost for recruiting new leaders, 'this value' * number_of_existing_leaders_of_type
-NDefines.NPolitics.NAVY_LEADER_COST = 1;   				-- cost for recruiting new leaders, 'this value' * number_of_existing_leaders_of_type
+NDefines.NPolitics.ARMY_LEADER_COST = 1;
+NDefines.NPolitics.NAVY_LEADER_COST = 1;
 
 NDefines.NBuildings.MAX_SHARED_SLOTS = 50
+
+-- added by Jay
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 75
+
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 28
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 200.0
+NDefines.NCountry.STARTING_COMMAND_POWER = 50.0
+NDefines.NCountry.ATTACHE_XP_SHARE = 0.05
+NDefines.NCountry.BASE_MOBILIZATION_SPEED = 0.02
+
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 60.0
+
+--Jay xp changes, a lot more for minors, little less for majors
+NDefines.NMilitary.LEADER_SKILL_FACTOR = 0.25
+NDefines.NMilitary.ARMY_LEADER_XP_GAIN_PER_UNIT_IN_COMBAT = 0.04
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1.00
+NDefines.NMilitary.FIELD_MARSHAL_XP_RATIO = 0.5
+
+NDefines.NMilitary.GARRISON_ORDER_ARMY_CAP_FACTOR = 1.5
+NDefines.NMilitary.XP_GAIN_PER_OVERRUN_UNIT = 10.0
+NDefines.NMilitary.XP_GAIN_FOR_SHATTERING = 10.0
+NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.01
+
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 20
